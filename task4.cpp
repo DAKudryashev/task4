4889389a94210f2ae41a4702c8b9f66413ab63cb
DAKudryashev <145067538+DAKudryashev@users.noreply.github.com>
2023-12-19 23:25:33 +0300

    Add files via upload

---
+++ task4.cpp
@@ -0,0 +1,59 @@
+﻿#include "task4.h"
+
+int main(){
+    List<int> list;
+
+    list.InsertNode(10, 0);
+    list.PrintList();
+    cout << "\n";
+
+    list.InsertNode(20, 0);
+    list.PrintList();
+    cout << "\n";
+
+    list.InsertNode(30, 0);
+    list.PrintList();
+    cout << "\n";
+
+    list.InsertNode(40, 0);
+    list.PrintList();
+    cout << "\n";
+
+    list.InsertNode(50, 1);
+    list.PrintList();
+    cout << "\n";
+
+    list.InsertNode(60, 3);
+    list.PrintList();
+    cout << "\n";
+
+    list.InsertNode(70, 100);
+    list.PrintList();
+    cout << "\n";
+
+    list.RemoveNode(0);
+    list.PrintList();
+    cout << "\n";
+
+    list.RemoveNode(4);
+    list.PrintList();
+    cout << "\n";
+
+    list.RemoveNode(4);
+    list.PrintList();
+    cout << "\n";
+
+    list.RemoveNode(20);
+    list.PrintList();
+    cout << "\n";
+
+    cout << list.GetElement(2) << "\n";
+    cout << "\n";
+
+    cout << list.GetElement(10) << "\n";
+    cout << "\n";
+
+    return 0;
+
+    
+}
---
+++ task4.h
@@ -0,0 +1,93 @@
+#pragma once
+
+#include <iostream>
+#include <optional>
+
+using namespace std;
+
+template <typename DataType>
+class List {
+private:
+	struct Node {
+		Node* next;
+		DataType data;
+	}* head;
+
+public:
+	List() {
+		head = nullptr;
+	}
+
+	List(List&&) { // move constructor
+
+	}
+
+	~List() {
+		while (head != nullptr) {
+			Node* current = head;
+			head = head->next;
+			delete current;
+		}
+	}
+
+	void InsertNode(DataType new_data, int position) { // При position>length-1 - в конец; при -1<position<length-1 - на место элемента со сдвигом вправо
+		Node* newNode = new Node;
+		newNode->data = new_data;
+		newNode->next = nullptr;
+		if (head == nullptr) {
+			head = newNode;
+		}
+		else if (position == 0) {
+			newNode->next = head;
+			head = newNode;
+		}
+		else if (position > 0) {
+			Node* i = head;
+			while (position > 1 and i->next != nullptr) {
+				i = i->next;
+				position--;
+			}
+			newNode->next = i->next;
+			i->next = newNode;
+		}
+	}
+
+	void RemoveNode(int position) { // При position>length-1 - ничего не менять; при -1<position<length-1 - исключить position
+		if (position == 0) {
+			head = head->next;
+		}
+		else if (position > 0) {
+			Node* i = head;
+			while (i->next != nullptr and position > 1) {
+				i = i->next;
+				position--;
+			}
+			if (i->next != nullptr){
+				i->next = i->next->next;
+			}
+		}
+		
+	}
+
+	DataType GetElement(int position) { // При position>length-1 - вернуть NULL; при -1<position<length-1 - position
+		Node* i = head;
+		while (i != nullptr and position > 0) {
+			i = i->next;
+			position--;
+		}
+		if (i != nullptr) { 
+			return i->data; 
+		}
+		else {
+			return NULL;
+		}
+	}
+
+	void PrintList() {
+		Node* i = head;
+		while (i != nullptr) {
+			cout << i->data << "\n";
+			i = i->next;
+		}
+	}
+};
